5a82d947670bdf2c3287e8af107159dc250fe7aa
Bork <lau_yh.22@ichat.sp.edu.sg>
2023-10-24 11:30:40 +0800

    V1.1 commit

--- Lab2.py
+++ Lab2.py
@@ -1,3 +1,4 @@
+"""
 def calculate_bmi(height, weight):
     print("Height = " + str(height))
     print("Weight = " + str(weight))
@@ -13,4 +14,53 @@ def calculate_bmi(height, weight):
     elif bmi > 25.0:
         print(f"Your BMI is {bmi2}. You are Overweight.")
 
-calculate_bmi(weight=57, height=1.73)
+    calculate_bmi(weight=57, height=1.73)
+"""
+
+
+def main():
+    display_main_menu()
+    user_input = get_user_input()
+    calc_average(user_input)
+    find_min_max(user_input)
+
+def display_main_menu():
+    print("Enter some numbers separated by commas e.g. 5, 67, 32")
+
+def get_user_input():
+    print("get_user_input")
+    input_str = input("Enter numbers separated by commas: ")
+    input_list = input_str.split(',')
+    input_list = [float(num) for num in input_list]
+    return input_list
+
+def calc_average(numbers):
+    print("calc_average")
+    no = len(numbers)
+    if no > 0:
+        average = sum(numbers) / no
+        print("Average:", average)
+    else:
+        print("No numbers to calculate the average.")
+
+# Call the main function to start the program
+
+
+def find_min_max(numbers):
+    print("find_min_max")
+    minimum=min(numbers)
+    maximum=max(numbers)
+    str(minimum)
+    str(maximum)
+    print("Max is ",maximum)
+    print("Min is ",minimum)
+
+
+
+def sort_tempereature():
+    print("sort_temperature")
+
+def calc_median_temperature():
+    print("calc_median_temperature")
+if __name__ == "__main__":
+    main()
