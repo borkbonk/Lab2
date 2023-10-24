34096e1ee1578e682349557093c264938c42da62
Bork <lau_yh.22@ichat.sp.edu.sg>
2023-10-24 10:22:57 +0800

    Update code to calculate bmi

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,16 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    # Calculate BMI
+    bmi = weight / (height * height)
+    bmi2 = f"{bmi:.2f}"  # Corrected formatting
+
+    if bmi < 18.5:
+        print(f"Your BMI is {bmi2}. You are Underweight.")
+    elif 18.5 <= bmi <= 25.0:
+        print(f"Your BMI is {bmi2}. You are Normal Weight.")
+    elif bmi > 25.0:
+        print(f"Your BMI is {bmi2}. You are Overweight.")
+
+calculate_bmi(weight=57, height=1.73)
